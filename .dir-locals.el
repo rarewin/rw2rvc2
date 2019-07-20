9234c7332a74b0981510790873a67c3a11a21a75
Katsuki Kobayashi <rare@tirasweel.org>
2019-07-20 14:29:28 +0900

    fix .dir-locals.el
      - <https://emacs.stackexchange.com/questions/12433/use-dir-locals-el-to-append-to-before-save-hook-as-a-buffer-local-variable>

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,5 @@
 	((c-file-style . "linux")
 	 (indent-tabs-mode . t)
 	 (tab-width . 8)
-	 (eval . (add-hook 'before-save-hook 'clang-format-buffer))
+	 (eval add-hook 'before-save-hook #'clang-format-buffer nil t)
 	 )))
